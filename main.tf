9a70f13cd2d9418f34fcfc57fea2f614cf0554b8
Rohan Naphade <110978112+robin0904@users.noreply.github.com>
2024-11-18 11:38:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_key_pair" "master_key_pair" {
 
 # Windows Server instance with dynamic username and session setup
 resource "aws_instance" "CentOS8-AMD" {
-  ami               = "ami-00b84670be6b17d8e"  # Replace with your desired CentOS AMI ID
+  ami               = "ami-0e51044ae4e4e5ef5"  # Replace with your desired CentOS AMI ID
   instance_type     = "c6a.xlarge"            # Replace with your desired instance type
   key_name          = aws_key_pair.master_key_pair.key_name
   subnet_id         = "subnet-09c6010c6cbfd6a17"

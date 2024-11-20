76ab5528d9ade0a05d7d1c07e7d7f8e24c74a38c
Rohan Naphade <110978112+robin0904@users.noreply.github.com>
2024-11-20 13:15:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "keypair_name" {
   type = string
-  default = "ssh_key"
 }
 
 # Instance name for tagging the Windows server
 variable "instance_name" {
   description = "Name tag for the Windows server instance"
   type        = string
-  default  = "SumedhaIT-server"
 }

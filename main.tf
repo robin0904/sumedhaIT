1c621bda608b6046a7aa2a6a7afda0dd74076cd6
Koushal Saini <koushal.saini@operisoft.com>
2025-07-28 14:42:57 +0530

    ami swapped - due to incorrect session creation

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_key_pair" "master_key_pair" {
 
 # Windows Server instance with dynamic username and session setup
 resource "aws_instance" "CentOS8-AMD" {
-  ami                    = "ami-0d2028fde0229318d" # Replace with your desired CentOS AMI ID
+  ami                    = "ami-0e21c1d2051dcf1d1" # Replace with your desired CentOS AMI ID
   instance_type          = var.instance_type       # Replace with your desired instance type
   key_name               = aws_key_pair.master_key_pair.key_name
   subnet_id              = "subnet-01e7e581424a68b10"

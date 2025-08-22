e1a65fdf2b697b0a02a709cb794cfaf556dafd19
Koushal Saini <koushal.saini@operisoft.com>
2025-08-22 13:02:40 +0530

    updated image with more AD configs

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_key_pair" "master_key_pair" {
 
 # Windows Server instance with dynamic username and session setup
 resource "aws_instance" "CentOS8-AMD" {
-  ami                    = "ami-01321b7b1254de77c" # Replace with your desired CentOS AMI ID
+  ami                    = "ami-0566c634de748abb0" # Replace with your desired CentOS AMI ID
   instance_type          = var.instance_type       # Replace with your desired instance type
   key_name               = aws_key_pair.master_key_pair.key_name
   subnet_id              = "subnet-01e7e581424a68b10"
